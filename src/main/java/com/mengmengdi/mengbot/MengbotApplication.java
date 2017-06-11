90df56cbd9671331425b90c273ef57073e11838e
Yik Wai Ng Jason <ywng@MacBook-Pro.local>
2017-06-11 20:49:35 +0800

    change packaging to war for wildfly.

--- src/main/java/com/mengmengdi/mengbot/MengbotApplication.java
+++ src/main/java/com/mengmengdi/mengbot/MengbotApplication.java
@@ -1,15 +1,10 @@
 package com.mengmengdi.mengbot;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
