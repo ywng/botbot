367faba2b85ad3410a45cd4796f5313f16874047
Yik Wai Ng Jason <ywng@MacBook-Pro.local>
2017-06-11 18:45:06 +0800

    change packaging to war for wildfly.

--- src/main/java/com/mengmengdi/mengbot/WebCrawler/CrawlerController.java
+++ src/main/java/com/mengmengdi/mengbot/WebCrawler/CrawlerController.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class CrawlerController {
 
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello World";
-    }
-
     @RequestMapping("/getStockPrice/{sid}")
     public double getStockPrice( @PathVariable("sid") String sid ) {
         double price = 0.0;
