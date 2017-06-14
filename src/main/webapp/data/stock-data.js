8132190a9d5d48769d607cc398ed4f311599344e
Yik Wai Ng Jason <ywng@MacBook-Pro.local>
2017-06-15 02:22:44 +0800

    Add Stock Portfolio Vis.

---
+++ src/main/webapp/data/stock-data.js
@@ -0,0 +1,200 @@
+/**
+ * Created by ywng on 15/6/2017.
+ */
+
+let data = [
+    {
+        cat: 'Technology',
+        name: '百奧家庭互動',
+        code: 02100,
+        price: 0.69,
+        quant: 84000,
+        mktVal: 57960
+    },
+    {
+        cat: 'Gambling',
+        name: '金沙中國有限公司',
+        code: 01928 ,
+        price: 37.2,
+        quant: 4400,
+        mktVal: 163460
+    },
+    {
+        cat: 'Gambling',
+        name: '澳博控股',
+        code: 00880,
+        price: 8.5,
+        quant: 2000,
+        mktVal: 17000
+    },
+    {
+        cat: 'Medical',
+        name: '中國先鋒醫藥',
+        code: 01345,
+        price: 2.39,
+        quant: 18000,
+        mktVal: 43020
+    },
+    {
+        cat: 'Medical',
+        name: '康臣葯業',
+        code: 01681,
+        price: 5.67,
+        quant: 9000,
+        mktVal: 51030
+    },
+    {
+        cat: 'Utilities',
+        name: '華能國際電力股份',
+        code: 00902,
+        price: 5.78,
+        quant: 10000,
+        mktVal: 57800
+    },
+    {
+        cat: 'Utilities',
+        name: '華電國際電力股份',
+        code: 01071,
+        price: 3.46,
+        quant: 8000,
+        mktVal: 37440
+    },
+    {
+        cat: 'Utilities',
+        name: '港燈—ＳＳ',
+        code: 02638,
+        price: 7.17,
+        quant: 5500,
+        mktVal: 39435
+    },
+    {
+        cat: 'Utilities',
+        name: '中廣核電力',
+        code: 01816,
+        price: 2.24,
+        quant: 24000,
+        mktVal: 53760
+    },
+    {
+        cat: 'Utilities',
+        name: '華能新能源',
+        code: 00958,
+        price: 2.44,
+        quant: 16000,
+        mktVal: 39040
+    },
+    {
+        cat: 'Construction & Decoration',
+        name: '泛亞國際',
+        code: 06128,
+        price: 1.79,
+        quant: 12000,
+        mktVal: 21480
+    },
+    {
+        cat: 'Transportation',
+        name: '中國中車',
+        code: 01766,
+        price: 7.26,
+        quant: 5000,
+        mktVal: 36300
+    },
+    {
+        cat: 'Technology Hardware',
+        name: '聯想集團',
+        code: 00992,
+        price: 5.13,
+        quant: 18000,
+        mktVal: 92340
+    },
+    {
+        cat: 'Financial Services',
+        name: '匯豐控股',
+        code: 00005,
+        price: 60,
+        quant: 800,
+        mktVal: 48000
+    },
+    {
+        cat: 'Logistics/Trade',
+        name: '利豐',
+        code: 00494,
+        price: 2.89,
+        quant: 10000,
+        mktVal: 28900
+    },
+    {
+        cat: 'Petroleum & Gases',
+        name: '中海油田服務',
+        code: 02883,
+        price: 6.58,
+        quant: 8000,
+        mktVal: 52640
+    },
+    {
+        cat: 'Petroleum & Gases',
+        name: '中國石油股份',
+        code: 00857,
+        price: 5.02,
+        quant: 10000,
+        mktVal: 50200
+    },
+    {
+        cat: 'Petroleum & Gases',
+        name: '中國石油化工股份',
+        code: 00386,
+        price: 5.107,
+        quant: 6000,
+        mktVal: 38880
+    },
+    {
+        cat: 'Petroleum & Gases',
+        name: '中國海洋石油',
+        code: 00883,
+        price: 8.75,
+        quant: 4000,
+        mktVal: 35000
+    },
+    {
+        cat: 'Real Estate',
+        name: '越秀房產信託基金',
+        code: 00405,
+        price: 4.68,
+        quant: 8000,
+        mktVal: 37440
+    },
+    {
+        cat: 'Insurance',
+        name: '中國財險',
+        code: 02328,
+        price: 12.7,
+        quant: 4000,
+        mktVal: 50720
+    },
+    {
+        cat: 'Insurance',
+        name: '中國太保',
+        code: 02601,
+        price: 30,
+        quant: 1200,
+        mktVal: 36000
+    },
+    {
+        cat: 'Coal',
+        name: '中國神華',
+        code: 01088,
+        price: 19.5,
+        quant: 2000,
+        mktVal: 39040
+    },
+    {
+        cat: 'Inv & Assest Management',
+        name: '中國信達',
+        code: 01359,
+        price: 2.92,
+        quant: 13000,
+        mktVal: 37960
+    }
+
+
+];
